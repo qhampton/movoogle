50f2dbf2faf7205a0e9307566a221f1f9e981118
Quinlan Hampton <qnhampton93@gmail.com>
2019-08-10 13:33:16 -0700

    adding google key for maps in key file

--- assets/javascript/apiKey.js
+++ assets/javascript/apiKey.js
@@ -1,2 +1,5 @@
 //yelp api key (qh)
 var yelpKey = "6I5kwV38QGc7oeIFQiuECG9wClMtE_z3JRjH_A2vZogTXe38W59haPoMSjSxt7k7_O2ytaM8aJd8BXIILhsmwRXR9eTqHnpZPAUiUzjSiMPT3etmD6Ej9vSRYTpKXXYx"
+
+//google api key (qh)
+var mapKey = "AIzaSyDb806mtzn8-73o4-cuXUsvoLx2j73g8TQ"
